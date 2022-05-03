3abd83af71b1768ffad6b1af09992c11849209ee
Aakash <123aakashsharma@gmail.com>
2022-05-03 17:26:26 +0530

    0.8.2

--- nodejs/package.json.gen.js
+++ nodejs/package.json.gen.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const common = {
-  version: "0.8.1",
+  version: "0.8.2",
   main: "index.js",
   types: "index.d.ts",
   files: ["cli.js", "postinstall.js", "index.d.ts", "index.js", "binaries"],
